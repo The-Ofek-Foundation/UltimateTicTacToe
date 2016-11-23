9ae95e805e0b35dba0379cb573c36fb3305725ab
Ofek Gila <ofek@theofekfoundation.org>
2016-11-23 14:37:40 -0800

    Use session data

--- script.js
+++ script.js
@@ -44,10 +44,10 @@ function pageReady() {
 	setTimeout(resizeGameSettingsTable, 0);
 
 	setTimeout(function() {
-		var explainSettings = getLocallyStored('settingsExplained');
+		var explainSettings = getSessionData('settingsExplained');
 		if (!explainSettings) {
 			alert("Type 's' to change your settings or 'n' to create a new game!");
-			setLocallyStored('settingsExplained', true);
+			setSessionData('settingsExplained', true);
 		}
 	}, 100);
 };
@@ -664,24 +664,6 @@ function getMCTSDepthRange() {
 		range[2] = "O";
 	else range[2] = "Tie";
 	return range;
-}
-
-function setCookie(cname, cvalue, exdays) {
-		var d = new Date();
-		d.setTime(d.getTime() + (exdays*24*60*60*1000));
-		var expires = "expires="+d.toUTCString();
-		document.cookie = cname + "=" + cvalue + "; " + expires;
-}
-
-function getCookie(cname) {
-		var name = cname + "=";
-		var ca = document.cookie.split(';');
-		for(var i=0; i<ca.length; i++) {
-				var c = ca[i];
-				while (c.charAt(0)===0) c = c.substring(1);
-				if (c.indexOf(name) === 0) return c.substring(name.length, c.length);
-		}
-		return "";
 }
 
 function MCTSGetChildren(father, tboard) {
